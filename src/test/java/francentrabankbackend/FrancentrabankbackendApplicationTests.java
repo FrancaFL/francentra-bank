ac51877850fab9a9e984003451ce7974222206dd
Franca Lauscher <165918979+FrancaFL@users.noreply.github.com>
2026-06-07 06:25:28 -0500

    Initial Spring Boot backend setup

---
+++ src/test/java/francentrabankbackend/FrancentrabankbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package francentrabankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancentrabankbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
